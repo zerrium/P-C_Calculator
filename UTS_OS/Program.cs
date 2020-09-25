df5f16f0a83a1447333407a7c3d6dffef3c5c892
zerrium <willysusilo1@gmail.com>
2020-09-25 13:50:28 +0700

    Initial commit

---
+++ UTS_OS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Gtk;
+
+namespace UTS_OS
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            MainWindow win = new MainWindow();
+            win.Show();
+            Application.Run();
+        }
+    }
+}
