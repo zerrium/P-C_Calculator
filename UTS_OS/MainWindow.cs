f65623b87dfaedcd877181f5c4903b425f600772
zerrium <willysusilo1@gmail.com>
2020-09-25 14:37:57 +0700

    Added radiobutton

--- UTS_OS/MainWindow.cs
+++ UTS_OS/MainWindow.cs
@@ -3,10 +3,7 @@ using Gtk;
 
 public partial class MainWindow : Gtk.Window
 {
-    public MainWindow() : base(Gtk.WindowType.Toplevel)
-    {
-        Build();
-    }
+    public MainWindow() : base(Gtk.WindowType.Toplevel) => Build();
 
     protected void OnDeleteEvent(object sender, DeleteEventArgs a)
     {
