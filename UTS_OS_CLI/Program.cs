1ce94fde05c0986b61c5e651d7ae5a8e4e5810b2
zerrium <willysusilo1@gmail.com>
2020-09-27 10:31:57 +0700

    Initial commit for CLI

---
+++ UTS_OS_CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UTS_OS_CLI
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
